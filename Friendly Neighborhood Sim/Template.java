fdcf0c6911036a1683f6db2fabecdc669ce731be
Halestorm928 <wyatthales@gmail.com>
2016-10-14 18:58:02 -0400

    Tempate added

---
+++ Friendly Neighborhood Sim/Template.java
@@ -0,0 +1,18 @@
+/**
+ * Template
+ * Display a rainbow on Finch
+ * Author: Wyatt Hales
+ * Date: 10/10
+ * I worked on this assignment alone, using only course materials.
+ **/
+ 
+import java.util.Scanner;
+import java.awt.Color;
+
+public class Template
+{
+    public static void main(String args[])
+    {
+        
+    }   
+}
