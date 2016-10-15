7293e1954bd049d9fbc3a45854a6476248a13eb9
homeofthemeep <tally.robinson360@gmail.com>
2016-10-14 23:47:00 -0400

    setParam Works

--- Friendly Neighborhood Sim/setParam.java
+++ Friendly Neighborhood Sim/setParam.java
@@ -2,26 +2,28 @@ import java.util.Scanner;
 //This class sets the parameters of a Nigger(class)
 public class setParam
 {
-    double rand;
-    byte randB;
+    private double rand = 0.0;
+    private byte randB = 0;
     public void randNumGen()
     {
         rand = Math.random();
         rand = rand * 100.0;
         randB = (byte)(Math.ceil(rand));
     }
-    randB = randB;
-    if (randB <= 33)
+    public void setNigger()
     {
-        Nigger n1 = new Nigger(0, 40, 10, 80, 90, 90, "John", 1);
-    }
-    else if(randB <= 67)
-    {
-        Nigger n2 = new Nigger(0, 30, 20, 110, 100, 100, "Barry", 1);
-    }
-    else if(randB <= 100)
-    {
-        Nigger n3 = new Nigger(0, 35, 25, 75, 115, 115, "Jarius", 1);
+        if (randB <= 33)
+        {
+            Nigger n1 = new Nigger(0, 40, 10, 80, 90, 90, "John", 1);
+        }
+        else if(randB <= 67)
+        {
+            Nigger n2 = new Nigger(0, 30, 20, 110, 100, 100, "Barry", 1);
+        }
+        else if(randB <= 100)
+        {
+            Nigger n3 = new Nigger(0, 35, 25, 75, 115, 115, "Jarius", 1);
+        }
     }
 }
     
