7d136bba14db7de94290a79966e48ab31a39da97
homeofthemeep <tally.robinson360@gmail.com>
2016-10-14 23:40:47 -0400

    khvg

--- Friendly Neighborhood Sim/setParam.java
+++ Friendly Neighborhood Sim/setParam.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
-
+//This class sets the parameters of a Nigger(class)
 public class setParam
 {
-    public void setParam()
+    double rand;
+    byte randB;
+    public void randNumGen()
     {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter # of arms");
-        int a = input.nextInt();
-        System.out.println("Enter # of buttons");
-        int b = input.nextInt();
-        
-        Nigger nigger1= new Nigger(a, b);
+        rand = Math.random();
+        rand = rand * 100.0;
+        randB = (byte)(Math.ceil(rand));
+    }
+    randB = randB;
+    if (randB <= 33)
+    {
+        Nigger n1 = new Nigger(0, 40, 10, 80, 90, 90, "John", 1);
+    }
+    else if(randB <= 67)
+    {
+        Nigger n2 = new Nigger(0, 30, 20, 110, 100, 100, "Barry", 1);
+    }
+    else if(randB <= 100)
+    {
+        Nigger n3 = new Nigger(0, 35, 25, 75, 115, 115, "Jarius", 1);
     }
 }
     
