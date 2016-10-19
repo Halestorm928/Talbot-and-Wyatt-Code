4af37a3e872a20aaecf56ad9bed5045fb3e89f56
homeofthemeep <tally.robinson360@gmail.com>
2016-10-19 13:41:03 -0400

    Adds up to 10 niggers

--- Friendly Neighborhood Sim/setParam.java
+++ Friendly Neighborhood Sim/setParam.java
@@ -13,47 +13,56 @@ public class setParam
     }
     public Nigger setNewNigger(Nigger n)
     {
-        if (randB <= 17)
+        if (randB <= 10)
         {
             n = new Nigger(0, 40, 10, 80, 90, 90, "John", 1, 13, 50);
             return n;
         }
-        else if(randB <= 34)
+        else if(randB <= 20)
         {
             n = new Nigger(0, 30, 20, 110, 100, 100, "Barry", 1, 16, 60);
             return n;
         }
-        else if(randB <= 51)
+        else if(randB <= 30)
         {
             n = new Nigger(0, 35, 25, 75, 115, 115, "Jarius", 1, 19, 35);
             return n;
         }
-        else if (randB <= 68)
+        else if (randB <= 40)
         {
             n = new Nigger(0, 35, 25, 75, 115, 115, "Maxwell", 1, 19, 35);
             return n;
         }
-        else if (randB <= 85)
+        else if (randB <= 50)
         {
             n = new Nigger(0, 45, 15, 90, 100, 100, "Josh", 1, 14, 70);
             return n;
         }
-        else if (randB <= 90)
+        else if (randB <= 60)
         {
-            n = new Nigger(0, 45, 15, 90, 100, 100, "Ben Ahmadi", 1, 14, 70);
+            n = new Nigger(0, 70, 5, 95, 120, 120, "Ahmadi", 1, 23, 25);
         }
         
-        else if (randB <= 100)
+        else if (randB <= 70)
         {
             n = new Nigger(0, 50, 30, 60, 90, 90, "Mehki", 1, 12, 75);
             return n;
         }
-        else if (randB <= 101)
+        else if (randB <= 80)
         {
             n = new Nigger(0, 30, 0, 65, 120, 120, "Evan", 1, 20, 25);
             return n;
         }
-        
+        else if (randB <= 90)
+        {
+            n = new Nigger(0, 40, 20, 80, 115, 115, "Thomas", 1, 21, 65);
+            return n;
+        }
+        else if (randB <= 100)
+        {
+            n = new Nigger(0, 40, 0, 55, 100, 100, "Gustav", 1, 16, 50);
+            return n;
+        }
         return n;
     }
 }
